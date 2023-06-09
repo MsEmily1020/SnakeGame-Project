9c0414ade6eb6603752a0d3e091d57e2212caadb
MsEmily1020 <peace10200khs@gmail.com>
2023-06-09 22:37:14 +0900

    [Feat] 좌표 체계 그리드화

--- SnakeGame/main.cpp
+++ SnakeGame/main.cpp
@@ -8,24 +8,28 @@ int main() {
 	const int WIDTH = 1000;
 	const int HEIGHT = 800;
 
+	int block = 40; // 한 칸을 40 으로
+	const int w = WIDTH / block;
+	const int h = HEIGHT / block;
+
 	RenderWindow  window(VideoMode(WIDTH, HEIGHT), "Snake Game");
-	// 1초에 60번의 작업이 이루어 지도록 frame 조절
+	// 1초에 30번의 작업이 이루어 지도록 frame 조절
 	// 컴퓨터 사양이 달라도 똑같이 행동 함
-	window.setFramerateLimit(60);
+	window.setFramerateLimit(30);
 
 	srand(time(NULL));
 
-
-	// 한 칸을 40 으로
-	int block = 40;
-
 	RectangleShape snake;
-	snake.setPosition(200, 300);
+	int snake_x = 3;
+	int snake_y = 3;
+	snake.setPosition(snake_x * block, snake_y * block);
 	snake.setSize(Vector2f(block, block));
 	snake.setFillColor(Color::Green);
 
 	RectangleShape apple;
-	apple.setPosition(rand() % WIDTH - block, rand() % HEIGHT - block);
+	int apple_x = rand() % w;
+	int apple_y = rand() % h;
+	apple.setPosition(apple_x * block, apple_y * block);
 	apple.setSize(Vector2f(block, block));
 	apple.setFillColor(Color::Red);
 
@@ -40,32 +44,46 @@ int main() {
 		}
 
 		if (Keyboard::isKeyPressed(Keyboard::Up))
-			snake.move(0, -5);
+		{
+			snake_y--;
+			snake.move(0, -block);
+		}
 
 		else if (Keyboard::isKeyPressed(Keyboard::Down))
-			snake.move(0, 5);
+		{
+			snake_y++;
+			snake.move(0, block);
+		}
 
 		else if (Keyboard::isKeyPressed(Keyboard::Left))
-			snake.move(-5, 0);
+		{
+			snake_x--;
+			snake.move(-block, 0);
+		}
 
 		else if (Keyboard::isKeyPressed(Keyboard::Right))
-			snake.move(5, 0);
+		{
+			snake_x++;
+			snake.move(block, 0);
+		}
 
 		// update
 
 		// 뱀이 사과를 먹었을 때
 		if (snake.getGlobalBounds().intersects(apple.getGlobalBounds()))
 		{
-			apple.setPosition(rand() % WIDTH - block, rand() % HEIGHT - block);
+			apple_x = rand() % w;
+			apple_y = rand() % h;
+			apple.setPosition(apple_x * block, apple_y * block);
 		}
 
-		// render	
+		// render
 		window.clear();
-
+		
 		window.draw(snake);
-
+		
 		window.draw(apple);	// 뱀과 사과가 겹칠경우 사과가 위에 나옴
-
+		
 		window.display();
 	}
 
