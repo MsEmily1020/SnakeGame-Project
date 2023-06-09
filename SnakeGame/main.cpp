f5cb6420235181ad71a9dd4b66925fe861c9bc49
MsEmily1020 <peace10200khs@gmail.com>
2023-06-09 22:33:13 +0900

    [Feat] 뱀이 사과를 먹었을 때 랜덤 위치 이동

--- SnakeGame/main.cpp
+++ SnakeGame/main.cpp
@@ -7,14 +7,16 @@ int main() {
 	// 1초에 60번의 작업이 이루어 지도록 frame 조절
 	// 컴퓨터 사양이 달라도 똑같이 행동 함
 	window.setFramerateLimit(60);
-	
+
+	srand(time(NULL));
+
 	RectangleShape snake;
 	snake.setPosition(200, 300);
 	snake.setSize(Vector2f(30, 30));
 	snake.setFillColor(Color::Green);
 
 	RectangleShape apple;
-	apple.setPosition(300, 400);
+	apple.setPosition(rand() % 640 - 30, rand() % 480 - 30);
 	apple.setSize(Vector2f(30, 30));
 	apple.setFillColor(Color::Red);
 
@@ -41,20 +43,22 @@ int main() {
 			snake.move(5, 0);
 		
 		// update
-
+		
 		// 뱀이 사과를 먹었을 때
 		if (snake.getGlobalBounds().intersects(apple.getGlobalBounds()))
 		{
-			apple.setPosition(-500, -500);
+			apple.setPosition(rand() % 640 - 30, rand() % 480 - 30);
 		}
 
 		// render
 		window.clear();
-
+		
 		window.draw(snake);
+		
 		window.draw(apple);	// 뱀과 사과가 겹칠경우 사과가 위에 나옴
-
+		
 		window.display();
 	}
+
 	return 0;
 }
