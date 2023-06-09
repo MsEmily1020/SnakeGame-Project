965ae0b7282975c40bd5e8652ae15cd97dd2a1c0
MsEmily1020 <peace10200khs@gmail.com>
2023-06-09 22:35:22 +0900

    [Feat] 값 변수화

--- SnakeGame/main.cpp
+++ SnakeGame/main.cpp
@@ -5,13 +5,18 @@
 using namespace sf;
 
 int main() {
-	RenderWindow  window(VideoMode(640, 480), "Snake Game");
+	const int WIDTH = 1000;
+	const int HEIGHT = 800;
+
+	RenderWindow  window(VideoMode(WIDTH, HEIGHT), "Snake Game");
 	// 1초에 60번의 작업이 이루어 지도록 frame 조절
 	// 컴퓨터 사양이 달라도 똑같이 행동 함
 	window.setFramerateLimit(60);
 
 	srand(time(NULL));
 
+
+	// 한 칸을 40 으로
 	int block = 40;
 
 	RectangleShape snake;
@@ -20,7 +25,7 @@ int main() {
 	snake.setFillColor(Color::Green);
 
 	RectangleShape apple;
-	apple.setPosition(rand() % 640 - block, rand() % 480 - block);
+	apple.setPosition(rand() % WIDTH - block, rand() % HEIGHT - block);
 	apple.setSize(Vector2f(block, block));
 	apple.setFillColor(Color::Red);
 
@@ -33,34 +38,34 @@ int main() {
 			if (e.type == Event::Closed)
 				window.close();
 		}
-		
+
 		if (Keyboard::isKeyPressed(Keyboard::Up))
 			snake.move(0, -5);
-		
+
 		else if (Keyboard::isKeyPressed(Keyboard::Down))
 			snake.move(0, 5);
-		
+
 		else if (Keyboard::isKeyPressed(Keyboard::Left))
 			snake.move(-5, 0);
-		
+
 		else if (Keyboard::isKeyPressed(Keyboard::Right))
 			snake.move(5, 0);
-		
+
 		// update
-		
+
 		// 뱀이 사과를 먹었을 때
 		if (snake.getGlobalBounds().intersects(apple.getGlobalBounds()))
 		{
-			apple.setPosition(rand() % 640 - block, rand() % 480 - block);
+			apple.setPosition(rand() % WIDTH - block, rand() % HEIGHT - block);
 		}
 
-		// render
+		// render	
 		window.clear();
-		
+
 		window.draw(snake);
-		
+
 		window.draw(apple);	// 뱀과 사과가 겹칠경우 사과가 위에 나옴
-		
+
 		window.display();
 	}
 
